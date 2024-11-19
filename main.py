db366acc8b9afaa59b14b7158a4109d9b04395ab
Somya <you@example.com>
2024-11-19 18:50:42 +0530

    hi

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from utils.fetch_data import fetch_inventory
 
 # Example Usage
 enqueue("12345", "Milk", 50, 10)
+enqueue("233423", "Egg", 70, 10)
+enqueue("564563", "Cake", 80, 10)
+enqueue("2342", "Sugar", 90, 10)
 dequeue("12345")
 
 # Generate a report
